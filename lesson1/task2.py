7c21296a372fb13a8a9e5bd93fa82770a83d7225
twist-tracer <twist.tracer@gmail.com>
2021-03-20 20:34:15 +0300

    <lesson1> task2

---
+++ lesson1/task2.py
@@ -0,0 +1,18 @@
+# По введенным пользователем координатам двух точек вывести уравнение прямой вида y = kx + b, проходящей через эти
+# точки.
+
+# диаграмма ->  https://app.diagrams.net/#G1humLnGoPjOVVo-ST4AwdSl2Bs7UiBcMC
+
+if __name__ == '__main__':
+    x1 = float(input('x1: '))
+    y1 = float(input('y1: '))
+
+    x2 = float(input('x2: '))
+    y2 = float(input('y2: '))
+
+    if x1 == x2:
+        print(f'x = {x1}')
+    else:
+        k = (y1 - y2) / (x1 - x2)
+        b = y2 - k * x2
+        print(f'y = {k} * x + {b}')
